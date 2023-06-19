f670e7931aeeec5e79032322afe168fdabe27583
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-06-19 15:07:45 +0700

    Инициализация проекта.

---
+++ src/main/java/ru/tsu/hits/kosterror/laundryemulator/LaundryEmulatorApplication.java
@@ -0,0 +1,13 @@
+package ru.tsu.hits.kosterror.laundryemulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryEmulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaundryEmulatorApplication.class, args);
+    }
+
+}
